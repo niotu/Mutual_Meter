7df180fd0bbf042452708e8e62f686d97dbd1b46
noitu <dimalego35@gmail.com>
2023-01-20 10:33:50 +0300

    clear data

--- clear_data.py
+++ clear_data.py
@@ -1,6 +1,7 @@
 import json
 
-default = {"highest_score": 0, "money": 0, "bought_cars": [], "current_car_index": 0}
+default = {"highest_score": 0, "money": 0, "bought_cars": ["red_car"], "current_car_index": 0,
+           "booster_levels": [1, 1, 1], "booster_stats": [1, 100, 1]}
 
 with open('storage/scores.json', 'w') as file:
     json.dump(default, file)
